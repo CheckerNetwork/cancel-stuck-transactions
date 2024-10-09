35e25a9581a823f956164d30a1483d84e1057a38
Julian Gruber <julian@juliangruber.com>
2024-10-09 10:47:05 +0200

    fix test, fix test nesting

--- test.js
+++ test.js
@@ -112,71 +112,74 @@ test('StuckTransactionsCanceller', async t => {
       assert.deepStrictEqual(sentTransactions, [])
       assert(storage.has(tx.hash))
     })
-  })
-  await t.test('cancel old transactions', async t => {
-    const tx = {
-      hash: 'hash',
-      maxPriorityFeePerGas: 10n,
-      gasLimit: 1n,
-      nonce: 20,
-      from: '0x0'
-    }
-    const storage = new Map()
-    const sentTransactions = []
-    const stuckTransactionsCanceller = new StuckTransactionsCanceller({
-      store: {
-        set ({ hash, timestamp, from, maxPriorityFeePerGas, gasLimit, nonce }) {
-          assert(!storage.has(hash))
-          storage.set(hash, {
-            hash,
-            timestamp,
-            from,
-            maxPriorityFeePerGas,
-            gasLimit,
-            nonce
-          })
-        },
-        list () {
-          return [...storage.values()]
-        },
-        remove (hash) {
-          assert(storage.has(hash))
-          assert.strictEqual(typeof hash, 'string')
-          storage.delete(hash)
-        }
-      },
-      log: str => {
-        // TODO: Test logs
-        debug(str)
-      },
-      sendTransaction (tx) {
-        sentTransactions.push(tx)
-        return {
-          hash: 'replacementTxHash',
-          wait: () => {}
-        }
+    await t.test('cancel old transactions', async t => {
+      const tx = {
+        hash: 'hash',
+        maxPriorityFeePerGas: 10n,
+        gasLimit: 1n,
+        nonce: 20,
+        from: '0x0'
       }
+      const storage = new Map()
+      const sentTransactions = []
+      const stuckTransactionsCanceller = new StuckTransactionsCanceller({
+        store: {
+          set ({ hash, timestamp, from, maxPriorityFeePerGas, gasLimit, nonce }) {
+            assert(!storage.has(hash))
+            storage.set(hash, {
+              hash,
+              timestamp,
+              from,
+              maxPriorityFeePerGas,
+              gasLimit,
+              nonce
+            })
+          },
+          list () {
+            return [...storage.values()]
+          },
+          remove (hash) {
+            assert(storage.has(hash))
+            assert.strictEqual(typeof hash, 'string')
+            storage.delete(hash)
+          }
+        },
+        log: str => {
+          // TODO: Test logs
+          debug(str)
+        },
+        sendTransaction (tx) {
+          sentTransactions.push(tx)
+          return {
+            ...tx,
+            hash: 'replacementTxHash',
+            wait: () => {}
+          }
+        }
+      })
+      await stuckTransactionsCanceller.addPending(tx)
+      await timers.setImmediate()
+      const status = await stuckTransactionsCanceller.cancelOlderThan(0)
+      assert.deepStrictEqual(status, [{
+        status: 'fulfilled',
+        value: undefined
+      }])
+      assert.strictEqual(sentTransactions.length, 1)
+      const sentTransactionClone = { ...sentTransactions[0] }
+      assert(sentTransactionClone.maxFeePerGas)
+      assert(sentTransactionClone.gasLimit)
+      delete sentTransactionClone.maxFeePerGas
+      delete sentTransactionClone.gasLimit
+      assert.deepStrictEqual(sentTransactionClone, {
+        maxPriorityFeePerGas: 13n,
+        nonce: tx.nonce,
+        from: tx.from,
+        to: tx.from,
+        value: 0
+      })
+      assert(storage.has('replacementTxHash'))
+      assert(!storage.has(tx.hash))
     })
-    await stuckTransactionsCanceller.addPending(tx)
-    await timers.setImmediate()
-    const status = await stuckTransactionsCanceller.cancelOlderThan(0)
-    assert.deepStrictEqual(status, [{
-      status: 'fulfilled',
-      value: undefined
-    }])
-    assert.strictEqual(sentTransactions.length, 1)
-    const sentTransactionClone = { ...sentTransactions[0] }
-    assert(sentTransactionClone.maxFeePerGas)
-    assert(sentTransactionClone.gasLimit)
-    delete sentTransactionClone.maxFeePerGas
-    delete sentTransactionClone.gasLimit
-    assert.deepStrictEqual(sentTransactionClone, {
-      maxPriorityFeePerGas: 13n,
-      nonce: tx.nonce,
-      to: tx.from,
-      value: 0
-    })
-    assert.deepStrictEqual(storage, new Map())
   })
   await t.test('#removeConfirmed()', async t => {
     const tx = {
